2dee41f72c312f1dc5a7c1f63253e8c3e8276b70
Stu Fletcher <mail@stuliveshere.com>
2014-10-07 10:32:20 +1000

    added multiprocessing

--- prac1/exersize6.py
+++ prac1/exersize6.py
@@ -2,24 +2,29 @@ from toolbox import io
 import toolbox
 import numpy as np
 import matplotlib.pyplot as pylab
+from multiprocessing import Pool
 from exersize1 import initialise
 from exersize5 import build_combined, add_noise, convolve_wavelet
+
+def run(sx):
+	#initialise 
+	print sx
+	#update workspace and parameter file
+	#clear workspace
+	#build the shot record
+	#convolve the wavelet
+	#add the noise
+	#add to output
+
+	#write to output	
 
 if __name__ == "__main__":
 	#initialise workspace, parameter file
 	
-
-	#iterate over shot points
-	for sx in param['sx_coords']:
-		print sx
-		#update workspace and parameter file
-		#clear workspace
-		#build the shot record
-		#convolve the wavelet
-		#add the noise
-		#add to output
-	
-		#write to output		
-		toolbox.cp(workspace, 'shot%d.su' %sx, **param)
-		
+	#allocate worker pool
+	pool = Pool(processes=8)
+	#run worker pool
+	result = pool.map(run, param['sx_coords'])
+	#workspace = toolbox.agc('.shot201.su', None, None)
+	#toolbox.display(workspace, None, None)	
 		
